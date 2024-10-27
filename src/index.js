c0421b6481f8dd662fb9ad794ee8b8c83a1d64e9
Thais Fernandes Ribeiro <146895816+tharibbeiro@users.noreply.github.com>
2024-10-27 12:55:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ if(pontuacaoHeroi < 1000) {
     nivelHeroi = "Ascendente"
 } else if (pontuacaoHeroi >= 9001 && pontuacaoHeroi <= 10000) {
     nivelHeroi = "Imortal"
-} else if (pontuacaoHeroi >= 10001) {
+} else (pontuacaoHeroi >= 10001) {
     nivelHeroi = "Radiante"
 }
 console.log("O Héroi de nome: " + nomeHeroi + " está no nível de " + nivelHeroi)
